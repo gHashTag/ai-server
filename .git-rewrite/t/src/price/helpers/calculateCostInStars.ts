d731224148f4745b57e2adae38c06a63750445fc
Dmitriy Vasilev <raoffonom@icloud.com>
2025-08-22 22:24:28 +0700

    checkpoint: Наведите порядок в документах. Все документы сл...

--- .git-rewrite/t/src/price/helpers/calculateCostInStars.ts
+++
@@ -1,5 +0,0 @@
-import { starCost } from './modelsCost'
-// Функция для расчета стоимости в звездах
-export function calculateCostInStars(costInDollars: number): number {
-  return costInDollars / starCost
-}
